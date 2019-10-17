9c28678d828329f53235ad139d05bbc35fff14a2
Lodhel <lodhelnew@gmail.com>
2019-10-17 21:19:40 +0300

    result type

--- api/services.py
+++ api/services.py
@@ -4,5 +4,17 @@ import re
 class MainServices:
 
     def check_data(self, number):
-        pass  # логика проверки данных полученных от пользователя
+        # логика проверки данных полученных от пользователя
+        pattern = re.compile('\d')
+        result = pattern.findall(number)
+        result = "".join(map(str, result))
 
+        result_type = "dms"
+        if len(result) == 12:
+            pass
+        elif len(result) == 10:
+            result_type = "oms"
+        return result_type
+
+
+MainServices().check_data("9876-543210")
